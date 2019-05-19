c0817553b69a032da394e2226a2b8369ffec216c
Vincent van Doesburg <vincent@MBP-van-Vincent.influx>
2019-05-19 22:24:36 +0200

    Gatsby v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi, I'm Vincent</h1>
+    <p>
+      You've reached my website, I'm not here right now but you can leave you
+      message after the beep.
+    </p>
   </Layout>
 )
 
